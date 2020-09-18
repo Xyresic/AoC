d7fd085c1fd0b5bfe0710b5876253adb84ced4bf
Xyresic <ericjmlam@gmail.com>
2020-09-17 20:26:45 -0400

    finished 2016.5

---
+++ 2016/solutions/Day5.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.security.*;
+
+public class Day5 {
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        return String.format("%0" + (bytes.length << 1) + "x", bi);
+    }
+
+    public static boolean isFull(String[] arr) {
+        for (String s : arr) {
+            if (s == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
+        String input = "uqwqemis";
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        StringBuilder code = new StringBuilder();
+        String[] code2 = new String[8];
+        int index = 1;
+
+        while (code.length() < 8) {
+            md.update((input + index).getBytes());
+            String hashed = toHex(md.digest());
+            if (hashed.startsWith("00000")) {
+                char pos6 = hashed.charAt(5);
+                code.append(pos6);
+                if (Character.isDigit(pos6)) {
+                    int ind = Character.getNumericValue(pos6);
+                    if (ind < 8 && code2[ind] == null) {
+                        code2[ind] = "" + hashed.charAt(6);
+                    }
+                }
+            }
+            index++;
+        }
+
+        while (!isFull(code2)) {
+            md.update((input + index).getBytes());
+            String hashed = toHex(md.digest());
+            if (hashed.startsWith("00000")) {
+                char pos6 = hashed.charAt(5);
+                if (Character.isDigit(pos6)){
+                    int ind = Character.getNumericValue(pos6);
+                    if (ind < 8 && code2[ind] == null) {
+                        code2[ind] = "" + hashed.charAt(6);
+                    }
+                }
+            }
+            index++;
+        }
+
+        //part one
+        System.out.println(code.toString());
+
+        //part two
+        for (String c : code2) {
+            System.out.print(c);
+        }
+    }
+}

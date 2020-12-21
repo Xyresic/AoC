f16dfe98c7371ca4c2223647ebab5ddcd61adc0f
Xyresic <ericjmlam@gmail.com>
2020-12-21 14:48:51 -0500

    finished 2017.1

---
+++ 2017/solutions/day1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+let summation = (a, b) => {return parseInt(a) + parseInt(b)};
+
+fs.readFile('../inputs/input1.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    data = data.trim();
+    let re = /(.)(?=\1)/g;
+    let sum = data.match(re).reduce(summation);
+    if (data.charAt(0) === data.slice(-1)) sum += parseInt(data.charAt(0));
+
+    //part one
+    console.log(sum);
+
+    let half = data.length / 2
+    re = new RegExp(`(.)(?=.{${half - 1}}\\1)`, 'g');
+    let looped = data.slice(half) + data.slice(0, half);
+    sum = data.match(re).reduce(summation);
+    sum += looped.match(re).reduce(summation);
+
+    //part two
+    console.log(sum);
+});

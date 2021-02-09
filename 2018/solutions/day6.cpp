a636d9e3ff7e169b3d692da1f741d0247b4dc1da
Xyresic <ericjmlam@gmail.com>
2021-02-09 14:32:58 -0500

    finished 2018.6

--- 2018/solutions/day6.cpp
+++ 2018/solutions/day6.cpp
@@ -3,22 +3,17 @@
 #include <vector>
 #include <regex>
 #include <algorithm>
+#include <numeric>
 
 struct Voronoi {
     int x, y, size;
     bool infinite;
-    std::vector<std::vector<int>> edge;
 
     Voronoi(int x, int y) {
         this->x = x;
         this->y = y;
-        this->size = 1;
+        this->size = 0;
         this->infinite = false;
-        this->edge.emplace_back(std::vector<int>{x,y});
-    }
-
-    bool operator !=(Voronoi& other) {
-        return this->x != other.x || this->y != other.y;
     }
 };
 
@@ -26,60 +21,39 @@ std::ifstream input("../inputs/input6.txt");
 std::string line;
 int bound_x = 0, bound_y = 0;
 
-bool contained(int x, int y, std::vector<std::vector<int>>& container) {
-    for (auto cell : container) if (cell[0] == x && cell[1] == y) return true;
-    return false;
-}
-
 int main(void) {
     std::vector<Voronoi> areas;
-    std::vector<std::vector<int>> visited;
+    int safe_size = 0;
     std::regex num("\\d+");
     while(getline(input, line)) {
         std::regex_iterator<std::string::iterator> rit(line.begin(), line.end(), num);
         int x = stoi(rit->str()), y = stoi((++rit)->str());
         areas.emplace_back(x, y);
-        visited.emplace_back(std::vector<int>{x, y});
         if (x > bound_x) bound_x = x;
         if (y > bound_y) bound_y = y;
     }
 
-    bool incomplete = true;
-    while (incomplete) {
-        incomplete = false;
-        for (Voronoi& area : areas) {
-            std::cout << "" << area.x << "," << area.y << " | ";
-            int edge_length = area.edge.size();
-            if (!area.infinite) {
-                for (int i = 0; i < edge_length; i++) {
-                    std::vector<int> cell = area.edge[i];
-                    for (int dx = -1; dx < 2; dx++) {
-                        for (int dy = dx == 0? -1:0; dy < 2; dy += 2) {
-                            int new_x = cell[0] + dx, new_y = cell[1] + dy;
-                            if (!contained(new_x, new_y, visited)) {
-                                visited.emplace_back(std::vector<int>{new_x, new_y});
-                                int dist = abs(area.x - new_x) + abs(area.y - new_y);
-                                bool min = true;
-                                for (Voronoi& other : areas) {
-                                    if (other != area && dist >= abs(other.x - new_x) + abs(other.y - new_y)) {
-                                        min = false;
-                                        break;
-                                    }
-                                }
-                                if (min) {
-                                    if (new_x <= 0 || new_x >= bound_x || new_y <= 0 || new_y >= bound_y) area.infinite = true;
-                                    area.size++;
-                                    area.edge.emplace_back(std::vector<int>{new_x, new_y});
-                                    incomplete = true;
-                                }
-                            }
-                        }
-                    }
-                }
+
+    for (int x = 0; x < bound_x + 1; x++) {
+        for (int y = 0; y < bound_y + 1; y++) {
+            std::vector<int> dists(areas.size());
+            std::transform(areas.begin(), areas.end(), dists.begin(), [x, y](Voronoi a){return abs(a.x - x) + abs(a.y - y);});
+            auto min = std::min_element(dists.begin(), dists.end());
+            if (std::count(dists.begin(), dists.end(), *min) == 1) {
+                areas[min - dists.begin()].size++;
+                if (x == 0 || x == bound_x || y == 0 || y == bound_y) areas[min - dists.begin()].infinite = true;
             }
+            if (std::accumulate(dists.begin(), dists.end(), 0) < 10000) safe_size++;
         }
     }
 
     //part one
-    std::cout << std::max_element(areas.begin(), areas.end(), [](auto a, auto b){return a.infinite || a.size < b.size;})->size << std::endl;
+    std::cout << std::max_element(areas.begin(), std::remove_if(areas.begin(), areas.end(), [](auto a){
+        return a.infinite;
+    }), [](auto a, auto b){
+        return a.size < b.size;
+    })->size << std::endl;
+
+    //part two
+    std::cout << safe_size << std::endl;
 }

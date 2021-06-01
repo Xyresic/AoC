c4c35652840f6f3efc300c249d92ce5ea46587d4
Xyresic <ericjmlam@gmail.com>
2021-06-01 17:24:40 -0400

    wrapped in a solution

--- 2019/solutions/day1/day1.cs
+++ 2019/solutions/day1/day1.cs
@@ -1,24 +1,31 @@
 using System;
 using System.IO;
 
-class Day1 {
-    static void Main(string[] args) {
-        string[] lines = File.ReadAllLines("../../inputs/input1.txt");
-        int sum = 0;
-        int fuel = 0;
-        foreach (string line in lines) {
-            int mass = int.Parse(line) / 3 - 2;
-            sum += mass;
-            while (mass > 0) {
-                fuel += mass;
-                mass = mass / 3 - 2;
+namespace Day1
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../inputs/input1.txt");
+            int sum = 0;
+            int fuel = 0;
+            foreach (string line in lines)
+            {
+                int mass = int.Parse(line) / 3 - 2;
+                sum += mass;
+                while (mass > 0)
+                {
+                    fuel += mass;
+                    mass = mass / 3 - 2;
+                }
             }
+
+            //part one
+            Console.WriteLine(sum);
+
+            //part two
+            Console.WriteLine(fuel);
         }
-
-        //part one
-        Console.WriteLine(sum);
-
-        //part two
-        Console.WriteLine(fuel);
-    }
+    }    
 }
--- 2019/solutions/day2/day2.cs
+++ 2019/solutions/day2/day2.cs
@@ -1,48 +1,59 @@
 ﻿using System;
 using System.IO;
 
-class Day2 {
-    static int run_intcode(int[] code, int noun, int verb) {
-        int[] intcode = (int[]) code.Clone();
-        int index = 0;
-        intcode[1] = noun;
-        intcode[2] = verb;
-        
-        while (index >= 0 && index < intcode.Length) {
-            switch (intcode[index]) {
-                case 1:
-                    intcode[intcode[index + 3]] = intcode[intcode[index + 1]] + intcode[intcode[index + 2]];
-                    index += 4;
-                    break;
-                case 2:
-                    intcode[intcode[index + 3]] = intcode[intcode[index + 1]] * intcode[intcode[index + 2]];
-                    index += 4;
-                    break;
-                case 99:
-                    index = -1;
-                    break;
-                default:
-                    index = -1;
-                    Console.WriteLine("Error");
-                    break;
+namespace Day2
+{
+    class Driver
+    {
+        static int run_intcode(int[] code, int noun, int verb)
+        {
+            int[] intcode = (int[]) code.Clone();
+            int index = 0;
+            intcode[1] = noun;
+            intcode[2] = verb;
+
+            while (index >= 0 && index < intcode.Length)
+            {
+                switch (intcode[index])
+                {
+                    case 1:
+                        intcode[intcode[index + 3]] = intcode[intcode[index + 1]] + intcode[intcode[index + 2]];
+                        index += 4;
+                        break;
+                    case 2:
+                        intcode[intcode[index + 3]] = intcode[intcode[index + 1]] * intcode[intcode[index + 2]];
+                        index += 4;
+                        break;
+                    case 99:
+                        index = -1;
+                        break;
+                    default:
+                        index = -1;
+                        Console.WriteLine("Error");
+                        break;
+                }
             }
+
+            return intcode[0];
         }
-        
-        return intcode[0];
-    }
-    
-    static void Main(string[] args) {
-        string lines = File.ReadAllText("../../inputs/input2.txt");
-        int[] intcode = Array.ConvertAll(lines.Split(','), s => int.Parse(s));
 
-        //part one
-        Console.WriteLine(run_intcode(intcode, 12, 2));
+        static void Main(string[] args)
+        {
+            string lines = File.ReadAllText("../../inputs/input2.txt");
+            int[] intcode = Array.ConvertAll(lines.Split(','), s => int.Parse(s));
 
-        //part two
-        for (int noun = 0; noun < 100; noun++) {
-            for (int verb = 0; verb < 100; verb++) {
-                if (run_intcode(intcode, noun, verb) == 19690720) {
-                    Console.WriteLine(noun * 100 + verb);
+            //part one
+            Console.WriteLine(run_intcode(intcode, 12, 2));
+
+            //part two
+            for (int noun = 0; noun < 100; noun++)
+            {
+                for (int verb = 0; verb < 100; verb++)
+                {
+                    if (run_intcode(intcode, noun, verb) == 19690720)
+                    {
+                        Console.WriteLine(noun * 100 + verb);
+                    }
                 }
             }
         }

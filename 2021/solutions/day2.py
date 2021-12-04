1d7ec8288b1813bd2a45fa028165aed6fcc44dec
Xyresic <ericjmlam@gmail.com>
2021-12-04 17:51:00 -0500

    refactored

--- 2021/solutions/day2.py
+++ 2021/solutions/day2.py
@@ -1,19 +1,25 @@
 with open('../inputs/input2.txt', 'r') as f:
     data = [s.strip().split(' ') for s in f.readlines()]
+
     pos1 = 0+0j
     pos2 = 0+0j
     aim = 0
     for inst in data:
         num = int(inst[1])
-        if inst[0] == 'forward':
-            pos1 += num
-            pos2 += num
-            pos2 += aim * num * 1j
-        elif inst[0] == 'down':
-            pos1 += num * 1j
-            aim += num
-        else:
-            pos1 -= num * 1j
-            aim -= num
+        match inst[0]:
+            case 'forward':
+                pos1 += num
+                pos2 += num
+                pos2 += aim * num * 1j
+            case 'down':
+                pos1 += num * 1j
+                aim += num
+            case _:
+                pos1 -= num * 1j
+                aim -= num
+
+    # part 1
     print(int(pos1.real * pos1.imag))
+
+    # part 2
     print(int(pos2.real * pos2.imag))

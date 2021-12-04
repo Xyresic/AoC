afcaa3ddcbd0df511fde571a9e1591d4fcbd901d
Xyresic <ericjmlam@gmail.com>
2021-12-04 17:45:56 -0500

    refactored

--- 2021/solutions/day1.py
+++ 2021/solutions/day1.py
@@ -1,13 +1,17 @@
 with open('../inputs/input1.txt', 'r') as f:
     data = [int(s.strip()) for s in f.readlines()]
 
-    # part 1
     count1 = 0
     count2 = 0
+    window = sum(data[0:3])
     for i in range(len(data) - 1):
-        if data[i+1] > data[i]:
+        if data[i + 1] > data[i]:
             count1 += 1
-        if i < len(data) - 2 and sum(data[i+1:i+4]) > sum(data[i:i+3]):
+        if i < len(data) - 3 and window < (window := window - data[i] + data[i + 3]):
             count2 += 1
+
+    # part 1
     print(count1)
+
+    # part 2
     print(count2)
